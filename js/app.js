bd27a997a2db42509932157070391258722a204d
Vincent Wu <wuv21@uw.edu>
2015-11-19 15:33:53 -0800

    lab

--- js/app.js
+++ js/app.js
@@ -8,5 +8,51 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    var canvas = document.getElementById('drawing-canvas');
+    var ctx = canvas.getContext('2d');
+    var input = document.getElementById('line-color-inp');
+    var undoBtn = document.getElementById('undo-btn');
+    var mouseDown = false;
+
+    canvas.addEventListener('mousedown', function(evt) {
+        ctx.beginPath();
+        ctx.moveTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+        mouseDown = true;
+    });
+
+    canvas.addEventListener('mousemove', function(evt) {
+        if (mouseDown === true) {
+            ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+            ctx.stroke();
+        }
+    });
+
+    canvas.addEventListener('mouseup', function(evt) {
+        ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
+        ctx.stroke();
+        mouseDown = false;
+
+        saveState();
+    });
+
+    var state;
+    function saveState() {
+        state = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        console.log(state);
+    }
+
+    input.addEventListener('change', function(evt) {
+        ctx.strokeStyle = input.value;
+        ctx.save();
+    });
+
+    function undo() {
+        ctx.drawImage(state, 0, 0);
+        console.log('undone');
+    }
+
+    undoBtn.addEventListener('click', undo);
+
+
 
 }); //DOMContentLoaded
