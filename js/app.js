a7c8f0888a9cdb4206de51e8659ac65a67bf3ab9
Dave Stearns <dlsinfo@uw.edu>
2015-11-18 21:48:08 -0800

    file stubs for lab exercise

--- js/app.js
+++ js/app.js
@@ -1,35 +1,11 @@
 /*
     Script for index.html
+
+    See the README.md file for instructions
+    https://github.com/drstearns/canvas-lab/blob/solution/README.md
 */
 
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
-    var canvas = document.querySelector('canvas');
-    var ctx = canvas.getContext('2d');
-    var mouseIsDown = false;
-    var colorInp = document.querySelector('#line-color-inp');
-
-    colorInp.addEventListener('change', function() {
-        ctx.strokeStyle = colorInp.value;
-    });
-
-    canvas.addEventListener('mousedown', function(evt) {
-        mouseIsDown = true;
-        ctx.beginPath();
-        ctx.moveTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
-    });
-
-    canvas.addEventListener('mousemove', function(evt) {
-        if (mouseIsDown) {
-            ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
-            ctx.stroke();
-        }
-    });
-
-    canvas.addEventListener('mouseup', function(evt) {
-        ctx.lineTo(evt.clientX - canvas.offsetLeft, evt.clientY - canvas.offsetTop);
-        ctx.stroke();
-        mouseIsDown = false;
-    });
-});
+}); //DOMContentLoaded
